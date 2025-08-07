67ab3a26586deb1ab3c83eb0e7738cb73cdc284d
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-08-06 22:04:32 -0600

    Add new validators and types

--- backend/src/utils/validator.ts
+++ backend/src/utils/validator.ts
@@ -10,7 +10,30 @@ export const NPERequestValidator = z.object({
 	npe: z.string().min(12, '').max(40, '').regex(/^\d+$/),
 });
 
+/**
+ * @description Main validator for Create Bills Lambda
+ */
+const PendingPaymentSchema = z.object({
+	AmountToPay: z.number(),
+	ServiceProvider: z.number(),
+	TemplateCode: z.string(),
+	CanExpire: z.boolean(),
+	ExpirationDate: z.number(),
+	ExtendedProperties: z.object({
+		UserContract: z.string(),
+	}),
+});
 
+/**
+ * @description Part of the validator
+ */
+export const CreateBillRequestValidatorSchema = z.object({
+	pendingPayments: z.array(PendingPaymentSchema).min(1, 'Please Send more elements'),
+});
+
+/**
+ *
+ */
 export const extendedSchema = APIGatewayProxyEventV2Schema.extend({
 	body: JSONStringified(NPERequestValidator),
 });
@@ -18,7 +41,12 @@ export const extendedSchema = APIGatewayProxyEventV2Schema.extend({
 export type NPERequestValidatorEvent = z.infer<typeof extendedSchema>;
 
 
-
+/**
+ * @description Request parser to provide decoded and typed data
+ * for the incoming event in the handler when is used @parser({ schema, safeParse: true }) decorator.
+ * @param validator
+ * @returns
+ */
 const genericRequestSchemaParser = <T extends z.ZodTypeAny>(validator: T) => {
 	return APIGatewayProxyEventV2Schema.extend({
 		body: z.string().transform(body => {
@@ -32,6 +60,23 @@ const genericRequestSchemaParser = <T extends z.ZodTypeAny>(validator: T) => {
 };
 
 
+/**
+ * @description
+ */
 export const payBillsRequestBodySchema = genericRequestSchemaParser(NPERequestValidator);
 
+/**
+ * @description
+ */
 export type payBillsRequestBodyType = z.infer<typeof payBillsRequestBodySchema>;
+
+/**
+ * @description Schemas for crete bills Lambda
+ */
+export const createBillsRequestBodySchema = genericRequestSchemaParser(CreateBillRequestValidatorSchema);
+
+/**
+ * @description Types for create bills Lambda
+ */
+export type createBillsRequestBodyType = z.infer<typeof createBillsRequestBodySchema>;
+export type PendingPaymentSchemaType = z.infer<typeof PendingPaymentSchema>;
